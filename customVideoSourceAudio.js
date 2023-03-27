48ec51908c95e8d19ef0fddafbaabd184939fff2
Ben Weekes <benweekes73@gmail.com>
2023-03-27 09:57:35 +0100

    Add lot of improvements

--- customVideoSourceAudio.js
+++ customVideoSourceAudio.js
@@ -1,3 +1,5 @@
+//Stats info https://www.w3.org/TR/webrtc-stats/
+
 const paramsString = document.location.search;
 const searchParams = new URLSearchParams(paramsString);
 const channel = searchParams.get("channel");
@@ -24,21 +26,32 @@ var peerConnection;
 var canv;
 var ctx;
 var loc;
-
 var start_p;
 var end_p;
 var start_a;
 var end_a;
-
 var br_a = {
   br: 0,
   brtime: 0
 }
-
 var br_p = {
   br: 0,
   brtime: 0
 }
+
+const { createWorker, createScheduler } = Tesseract;
+const scheduler = createScheduler();
+const video = document.getElementById('poem-video');
+const messages = document.getElementById('messages');
+
+let timerId = null;
+let samplecount = 0;
+let p2p_total = 0;
+let agora_total = 0;
+let p_min = 100000;
+let p_max = 0;
+let a_min = 100000;
+let a_max = 0;
 
 var callStatsMap_a = {
   renderFrameRate: 0,
@@ -49,13 +62,35 @@ var callStatsMap_a = {
   renderRateStdDeviationPercSum: 0,
   renderRateStdDeviationPercAvg: 0,
   renderRateStdDeviationPercCount: 0,
+  jitter: 0,
   jittersum: 0,
   jittercount: 0,
   jitteravg: 0,
   jittermax: 0,
-  renderRates: []
+  renderRates: [],
+  nack: 0,
+  kbps: 0,
+  framesDropped: 0,
+  freezeCount: 0,
+  totalFreezesDuration: 0,
+  packetsLost: 0,
+  bytesReceived: 0,
+  frameHeight: 0,
+  frameHeightSum: 0,
+  frameHeightCount: 0,
+  frameHeightAvg: 0,
+  frameWidth: 0,
+  frameWidthSum: 0,
+  frameWidthCount: 0,
+  frameWidthAvg: 0,
+  fps: 0,
+  fpsSum: 0,
+  fpsCount: 0,
+  fpsAvg: 0,
+  jitterBufferDelayAudio: 0,
+  packetsLostAudio: 0,
+  packetsDiscardedAudio: 0
 }
-
 
 var callStatsMap_p = {
   renderFrameRate: -1,
@@ -66,11 +101,34 @@ var callStatsMap_p = {
   renderRateStdDeviationPercSum: 0,
   renderRateStdDeviationPercAvg: 0,
   renderRateStdDeviationPercCount: 0,
+  jitter: 0,
   jittersum: 0,
   jittercount: 0,
   jitteravg: 0,
   jittermax: 0,
-  renderRates: []
+  renderRates: [],
+  nack: 0,
+  kbps: 0,
+  framesDropped: 0,
+  freezeCount: 0,
+  totalFreezesDuration: 0,
+  packetsLost: 0,
+  bytesReceived: 0,
+  frameHeight: 0,
+  frameHeightSum: 0,
+  frameHeightCount: 0,
+  frameHeightAvg: 0,
+  frameWidth: 0,
+  frameWidthSum: 0,
+  frameWidthCount: 0,
+  frameWidthAvg: 0,
+  fps: 0,
+  fpsSum: 0,
+  fpsCount: 0,
+  fpsAvg: 0,
+  jitterBufferDelayAudio: 0,
+  packetsLostAudio: 0,
+  packetsDiscardedAudio: 0
 }
 
 $.get('https://www.cloudflare.com/cdn-cgi/trace', function (data) {
@@ -83,7 +141,6 @@ $.get('https://www.cloudflare.com/cdn-cgi/trace', function (data) {
   document.getElementById("location").textContent = data.loc;
   document.getElementById("ip").textContent = data.ip;
   loc = data.loc;
-
 });
 
 let p2pids = channel + (100 + Math.floor(Math.random() * 100));
@@ -94,7 +151,6 @@ else
   peer = new Peer(p2pids, p2pice);
 
 var currentCall;
-
 peer.on("open", function (id) {
   document.getElementById("uuid").textContent = id;
   if (!rpid) {
@@ -120,7 +176,6 @@ peer.on('connection', function (conn) {
   });
 });
 
-
 var connect_p = -1;
 var connect_a = -1;
 
@@ -130,7 +185,7 @@ peer.on("call", async (call) => {
   currentCall = call;
   call.on("stream",async (remoteStream) => {
     document.getElementById("received-video").srcObject = remoteStream;
-//await document.getElementById("received-video").play();
+    //await document.getElementById("received-video").play();
     var stream = document.getElementById("received-video").captureStream(30);
     var newTrack = stream.getVideoTracks()[0];
     //currentCall.peerConnection.getSenders()[0].replaceTrack(newTrack);
@@ -141,6 +196,7 @@ peer.on("call", async (call) => {
 function webcam() {
   ctx.drawImage(myvideo, 0, 0, 1280, 720);
 }
+
 function clock() {
   ctx.font = '200px serif';
   var d = new Date();
@@ -149,7 +205,6 @@ function clock() {
   ctx.font = "200px Arial";
   ctx.fillText(d.toLocaleTimeString('en-US', { hour12: false }) + "." + d.getMilliseconds(), 30, 400);
   document.getElementById("gmt").textContent = d.toUTCString();
-
 }
 
 /*
@@ -181,7 +236,6 @@ var localTracks = {
 };
 
 var remoteUsers = {};
-
 var options = {
   appid: null,
   channel: null,
@@ -192,7 +246,6 @@ var options = {
 options.appid = "20b7c51ff4c644ab80cf5a4e646b0537";
 options.channel = channel; //"peer";
 
-
 var lstream;
 async function setStream() {
   if (usewebcam) {
@@ -202,33 +255,26 @@ async function setStream() {
     });
   }
 
-  //var strm= document.getElementById("canvas").captureStream(30);
   lstream= document.getElementById("canvas").captureStream(30);
-  
 }
 
 async function join() {
   client.on("user-published", handleUserPublished);
   client.on("user-unpublished", handleUserUnpublished);
-
   localTracks.audioTrack = await AgoraRTC.createMicrophoneAudioTrack();
   localTracks.videoTrack = await AgoraRTC.createCustomVideoTrack({ mediaStreamTrack: lstream.getVideoTracks()[0] });
   start_a = Date.now();
   options.uid = await client.join(options.appid, options.channel, options.token || null, options.uid || null);
   await client.publish([localTracks.videoTrack, localTracks.audioTrack]);
-  
 }
 
 async function join2() {
-
   client.on("user-published", handleUserPublished);
   client.on("user-unpublished", handleUserUnpublished);
-
   options.uid = await client.join(options.appid, options.channel, options.token || null, options.uid || null);
   //await client.publish(Object.values(localTracks));
   //localTracks.videoTrack.play("local-player");
 }
-
 
 async function subscribe(user, mediaType) {
   const uid = user.uid;
@@ -244,7 +290,6 @@ async function subscribe(user, mediaType) {
   if (mediaType === 'audio') {
     user.audioTrack.play();
   }
- 
 }
 
 async function ret() {
@@ -253,11 +298,10 @@ async function ret() {
     var stream = el.captureStream(30);
     var track = await AgoraRTC.createCustomVideoTrack({ mediaStreamTrack: stream.getVideoTracks()[0] });
     var audioTrack = await AgoraRTC.createMicrophoneAudioTrack();
-
     await client.publish([track, audioTrack]);
   }
-
 }
+
 function handleUserPublished(user, mediaType) {
   const id = user.uid;
   remoteUsers[id] = user;
@@ -295,7 +339,6 @@ if (rpid) {
 }
 
 async function setupP2P() {
- 
   let gstream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true });
   await gstream.addTrack(lstream.getVideoTracks()[0]);
   start_p = Date.now();
@@ -320,48 +363,58 @@ async function setupP2P() {
   currentCall = call;
 }
 
-function setStatsAudio(report, div, connect, brref, callStatsMap){
-  /*
-  jitter
-jitterBufferDelay
-jitterBufferEmittedCount
-jitterBufferMinimumDelay
-jitterBufferTargetDelay
-removedSamplesForAcceleration
-packetsLost
-packetsDiscarded
-packetsReceived
-*/
-var jitter = report["jitter"];
-var jitterBufferDelay = report["jitterBufferDelay"];
-var jitterBufferEmittedCount = report["jitterBufferEmittedCount"];
-var jitterBufferMinimumDelay = report["jitterBufferMinimumDelay"];
-var jitterBufferTargetDelay = report["jitterBufferTargetDelay"].toFixed(3);
-var removedSamplesForAcceleration = report["removedSamplesForAcceleration"];
+function setStatsAudio(report, mode, callStatsMap){
+
+var jitterBufferDelay = (report["jitterBufferDelay"]/report["jitterBufferEmittedCount"]).toFixed(3);
+if (jitterBufferDelay && jitterBufferDelay > 0 && !isNaN(jitterBufferDelay)) {
+  callStatsMap.jitterBufferDelayAudio=jitterBufferDelay;
+}
+
 var packetsLost = report["packetsLost"];
+if (packetsLost && packetsLost > 0 && !isNaN(packetsLost)) {
+  callStatsMap.packetsLostAudio=packetsLost;
+}
+
 var packetsDiscarded = report["packetsDiscarded"];
-var packetsReceived = report["packetsReceived"];
+if (packetsDiscarded && packetsDiscarded > 0 && !isNaN(packetsDiscarded)) {
+  callStatsMap.packetsDiscardedAudio=packetsDiscarded;
+}
 
-console.log("audio ",jitter,jitterBufferDelay,jitterBufferEmittedCount,jitterBufferMinimumDelay,jitterBufferTargetDelay,removedSamplesForAcceleration,packetsLost,packetsDiscarded,packetsReceived);
-
+//console.log("audio ",mode, jitter,jitterBufferDelay,jitterBufferEmittedCount,jitterBufferMinimumDelay,jitterBufferTargetDelay,removedSamplesForAcceleration,packetsLost,packetsDiscarded,packetsReceived);
+//console.log("audio ",mode, "jitterBufferDelay",jitterBufferDelay,"jitterBufferTargetDelay",jitterBufferTargetDelay,"packetsLost",packetsLost,"packetsDiscarded",packetsDiscarded);
 }
 
 // video
 function setStats(report, div, connect, brref, callStatsMap) {
-  var nack = report["nackCount"];
-  var fps = report["framesPerSecond"];
+
   var framesDropped = report["framesDropped"];
+  if (framesDropped && framesDropped > 0 && !isNaN(framesDropped)) {
+    callStatsMap.framesDropped=framesDropped;
+  }
+  
   var freezeCount = report["freezeCount"];
+  if (freezeCount && freezeCount > 0 && !isNaN(freezeCount)) {
+    callStatsMap.freezeCount=freezeCount;
+  }
+
   var totalFreezesDuration = report["totalFreezesDuration"];
-  var totalPausesDuration = report["totalPausesDuration"];
-  var totalSquaredInterFrameDelay = report["totalSquaredInterFrameDelay"].toFixed(3);
-  
-  console.log("video",framesDropped,freezeCount,totalFreezesDuration,totalPausesDuration,totalSquaredInterFrameDelay);
-  
-  var frameHeight = report["frameHeight"];
-  var frameWidth = report["frameWidth"];
-  var jitter = report["jitter"];//.toFixed(3);
+  if (totalFreezesDuration && totalFreezesDuration > 0 && !isNaN(totalFreezesDuration)) {
+    callStatsMap.totalFreezesDuration=totalFreezesDuration;
+  }
+
+  var packetsLost = report["packetsLost"];
+  if (packetsLost && packetsLost > 0 && !isNaN(packetsLost)) {
+    callStatsMap.packetsLost=packetsLost;
+  }
+
+  var bytesReceived = report["bytesReceived"];
+  if (bytesReceived && bytesReceived > 0 && !isNaN(bytesReceived)) {
+    callStatsMap.bytesReceived=bytesReceived;
+  }
+
+  var jitter = report["jitter"];
   if (jitter && jitter > 0 && !isNaN(jitter)) {
+    callStatsMap.jitter=jitter;
     callStatsMap.jittersum = jitter + callStatsMap.jittersum;
     callStatsMap.jittercount++;
     callStatsMap.jitteravg = callStatsMap.jittersum / callStatsMap.jittercount;
@@ -371,37 +424,63 @@ function setStats(report, div, connect, brref, callStatsMap) {
     if (jitter > callStatsMap.jittermax) {
       callStatsMap.jittermax = jitter;
     }
-  }
+  }  
   jitter = jitter.toFixed(3);
-  var jitterBufferDelay = report["jitterBufferDelay"].toFixed(2);
-  var jitterBufferEmittedCount = report["jitterBufferEmittedCount"].toFixed(0);
-  var packetsLost = report["packetsLost"];
-  var bytesReceived = report["bytesReceived"];
-  var packetsReceived = report["packetsReceived"];
 
+  var frameHeight = report["frameHeight"];
+  if (frameHeight && frameHeight > 0 && !isNaN(frameHeight)) {
+    callStatsMap.frameHeight=frameHeight;
+    callStatsMap.frameHeightSum = frameHeight + callStatsMap.frameHeightSum;
+    callStatsMap.frameHeightCount++;
+    callStatsMap.frameHeightAvg = (callStatsMap.frameHeightSum / callStatsMap.frameHeightCount).toFixed(0);
+  }
+
+  var frameWidth = report["frameWidth"];
+  if (frameWidth && frameWidth > 0 && !isNaN(frameWidth)) {
+    callStatsMap.frameWidth=frameWidth;
+    callStatsMap.frameWidthSum = frameWidth + callStatsMap.frameWidthSum;
+    callStatsMap.frameWidthCount++;
+    callStatsMap.frameWidthAvg = (callStatsMap.frameWidthSum / callStatsMap.frameWidthCount).toFixed(0);
+  }
+
+  var fps = report["framesPerSecond"];
+  if (fps && fps > 0 && !isNaN(fps)) {
+    callStatsMap.fps=fps;
+    callStatsMap.fpsSum = fps + callStatsMap.fpsSum;
+    callStatsMap.fpsCount++;
+    callStatsMap.fpsAvg = (callStatsMap.fpsSum/callStatsMap.fpsCount).toFixed(1);
+  }
+  
+  //console.log("video",div,"packetsLost",packetsLost,"framesDropped",framesDropped,"freezeCount",freezeCount,"totalFreezesDuration",totalFreezesDuration);
+    
   if (fps && !isNaN(fps))
     callStatsMap.renderFrameRate = fps;
   else
     callStatsMap.renderFrameRate = -1;
+
   calculateRenderRateVolatility(callStatsMap);
 
   var now = Date.now();
   var bb = brref.br;
   var br_delta = bytesReceived - brref.br;
   var brtime_delta = now - brref.brtime;
-
   brref.brtime = now;
   brref.br = bytesReceived;
 
-  var kbps = 0;
   if (bb > 0) {
-    kbps = (br_delta * 8 / 1000) / (brtime_delta / 1000);
+    callStatsMap.kbps = (br_delta * 8 / 1000) / (brtime_delta / 1000);
   }
 
-  //var stats = "Connect time: "+connect+" ms, W: "+frameWidth+", H: "+frameHeight+" <br>FPS: "+fps+", Nack: "+nack+", Lost: "+packetsLost+" kbps: "+Math.floor(kbps)+"  <br>Jitter: "+jitter+", avg:"+callStatsMap.jitteravg.toFixed(3)+", max: "+callStatsMap.jittermax.toFixed(3)+" <br>Jitter Delay: "+jitterBufferDelay+" <br>Jitter Emitted: "+jitterBufferEmittedCount+" <br>SquaredInterFrameDelay: "+totalSquaredInterFrameDelay+"<br>framesDropped: "+framesDropped+"<br>FPS Vol%: "+callStatsMap.renderRateStdDeviationPerc.toFixed(0)+", avg:"+callStatsMap.renderRateStdDeviationPercAvg.toFixed(0)+", max:"+callStatsMap.renderRateStdDeviationPercMax.toFixed(0) ;
+  var nack = report["nackCount"];
+  if (nack && nack > 0 && !isNaN(nack)) {
+    callStatsMap.nack=nack;
+  }
+
+  /*
   var stats = "Connect time: " + connect + " ms, W: " + frameWidth + ", H: " + frameHeight + " <br>FPS: " + fps + ", Nack: " + nack + ", Lost: " + packetsLost + " kbps: " + Math.floor(kbps) + "  <br>Jitter: " + jitter + ", avg:" + callStatsMap.jitteravg.toFixed(3) + ", max: " + callStatsMap.jittermax.toFixed(3) + "<br>FPS Vol%: " + callStatsMap.renderRateStdDeviationPerc.toFixed(0) + ", avg:" + callStatsMap.renderRateStdDeviationPercAvg.toFixed(0) + ", max:" + callStatsMap.renderRateStdDeviationPercMax.toFixed(0);
   //console.log(stats);
   document.getElementById(div).innerHTML = stats;
+  */
 }
 
 async function getStats() {
@@ -415,9 +494,8 @@ async function getStats() {
           setStats(report, "statsc2", connect_a, br_a, callStatsMap_a);
         }
         if (report.type === "inbound-rtp" && report.kind === "audio") {
-          setStatsAudio(report, "statsc2", connect_a, br_a, callStatsMap_a);
+          setStatsAudio(report, "sd-rtn", callStatsMap_a);
         }
-        //console.log(report.type,report);
       })
     })
   }
@@ -429,11 +507,44 @@ async function getStats() {
           setStats(report, "statsb2", connect_p, br_p, callStatsMap_p);
         }
         if (report.type === "inbound-rtp" && report.kind === "audio") {
-          setStatsAudio(report, "statsb2", connect_p, br_p, callStatsMap_p);
+          setStatsAudio(report, "p2p", callStatsMap_p);
         }
       })
     })
   }
+
+   /* Display Stats */
+  let connectt=connect_a;
+  let csm=callStatsMap_a;
+  let latency_avg_a= Math.floor(agora_total / samplecount);
+  let genius_score=(csm.fpsAvg*csm.frameWidthAvg*csm.frameHeightAvg)/(1+latency_avg_a+(10*csm.totalFreezesDuration)+csm.packetsDiscardedAudio);
+  
+  if (isNaN(genius_score)){
+    genius_score=0;
+  }
+  genius_score=genius_score.toFixed(0);
+  var stats = "Connect time: " + connectt + " ms, W: " + csm.frameWidth + ", H: " + csm.frameHeight + " <br>FPS: " + csm.fps + ", Nack: " + csm.nack + ", Lost: " + csm.packetsLost + " kbps: " + Math.floor(csm.kbps) + "  <br>Jitter: " + csm.jitter + ", avg:" + csm.jitteravg.toFixed(3) + ", max: " + csm.jittermax.toFixed(3) + "<br>FPS Vol%: " + csm.renderRateStdDeviationPerc.toFixed(0) + ", avg:" + csm.renderRateStdDeviationPercAvg.toFixed(0) + ", max:" + csm.renderRateStdDeviationPercMax.toFixed(0);
+  stats=stats+ " <br>Dropped: " + csm.framesDropped + ", Freezes: " + csm.freezeCount + ", Freeze Dur: " + csm.totalFreezesDuration ;
+  stats=stats+ " <br>Averages fps: " + csm.fpsAvg + ", W: " + csm.frameWidthAvg + ", H: " + csm.frameHeightAvg ;
+  stats=stats+ " <br>Audio lost: " + csm.packetsLostAudio + ", discard: " + csm.packetsDiscardedAudio + ", delay: " + csm.jitterBufferDelayAudio ;
+  stats=stats+ " <br><b>Genius Score</b> : " + genius_score;
+
+  document.getElementById("statsc2").innerHTML = stats;
+
+  connectt=connect_p;
+  csm=callStatsMap_p;
+  let latency_avg_p= Math.floor(p2p_total / samplecount);
+  genius_score=(csm.fpsAvg*csm.frameWidthAvg*csm.frameHeightAvg)/(1+latency_avg_p+(10*csm.totalFreezesDuration)+csm.packetsDiscardedAudio);
+  if (isNaN(genius_score)){
+    genius_score=0;
+  }
+  genius_score=genius_score.toFixed(0);
+  stats = "Connect time: " + connectt + " ms, W: " + csm.frameWidth + ", H: " + csm.frameHeight + " <br>FPS: " + csm.fps + ", Nack: " + csm.nack + ", Lost: " + csm.packetsLost + " kbps: " + Math.floor(csm.kbps) + "  <br>Jitter: " + csm.jitter + ", avg:" + csm.jitteravg.toFixed(3) + ", max: " + csm.jittermax.toFixed(3) + "<br>FPS Vol%: " + csm.renderRateStdDeviationPerc.toFixed(0) + ", avg:" + csm.renderRateStdDeviationPercAvg.toFixed(0) + ", max:" + csm.renderRateStdDeviationPercMax.toFixed(0);
+  stats=stats+ " <br>Dropped: " + csm.framesDropped + ", Freezes: " + csm.freezeCount + ", Freeze Dur: " + csm.totalFreezesDuration ;
+  stats=stats+ " <br>Averages fps: " + csm.fpsAvg + ", W: " + csm.frameWidthAvg + ", H: " + csm.frameHeightAvg ;
+  stats=stats+ " <br>Aud lost: " + csm.packetsLostAudio + ", discard: " + csm.packetsDiscardedAudio + ", delay: " + csm.jitterBufferDelayAudio ;
+  stats=stats+ " <br><b>Genius Score</b> : " + genius_score;
+  document.getElementById("statsb2").innerHTML = stats;
 }
 
 function getMS(parsedDate) {
@@ -446,20 +557,7 @@ function getMS(parsedDate) {
   return ms + hms + mms + sms;
 }
 
-const { createWorker, createScheduler } = Tesseract;
-const scheduler = createScheduler();
-const video = document.getElementById('poem-video');
-const messages = document.getElementById('messages');
 
-let timerId = null;
-
-let samplecount = 0;
-let p2p_total = 0;
-let agora_total = 0;
-let p_min = 100000;
-let p_max = 0;
-let a_min = 100000;
-let a_max = 0;
 
 const doOCR = async () => {
   const c = document.getElementById('canvas');
@@ -522,7 +620,7 @@ const doOCR = async () => {
     ms_agora = -1;
   }
 
-  console.log("ms_local", ms_local, "ms_p2p", ms_p2p, "ms_agora", ms_agora, "ms_local-ms_agora", ms_local - ms_agora, "ms_local-ms_p2p", ms_local - ms_p2p);
+  //console.log("ms_local", ms_local, "ms_p2p", ms_p2p, "ms_agora", ms_agora, "ms_local-ms_agora", ms_local - ms_agora, "ms_local-ms_p2p", ms_local - ms_p2p);
 
   if (ms_local > -1 && ms_p2p > -1 && ms_agora > -1) {
     let lp = (ms_local - ms_p2p);
@@ -594,17 +692,26 @@ function calculateRenderRateVolatility(statsMap) {
 
 (async () => {
   if (!rpid) {
+    console.warn(1);
     for (let i = 0; i < 4; i++) {
+      console.warn(10);
       const worker = createWorker();
+      console.warn(11);
       await worker.load();
+      console.warn(12);
       await worker.loadLanguage('eng');
+      console.warn(13);
       await worker.initialize('eng');
+      console.warn(14);
       scheduler.addWorker(worker);
+      console.warn(15);
     }
+    console.warn(2);
     timerId = setInterval(doOCR, 2000);
+    console.warn(3);
     setInterval(getStats, 1000);
+    console.warn(4);
+    document.getElementById("connect").disabled=false;
+    document.getElementById("connect").style.backgroundColor="#007bff";
   }
 })();
-
-
-
